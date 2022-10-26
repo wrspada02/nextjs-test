2a4616aaaaf98cc8941031b1d12c3330a43d8b97
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2022-10-25 23:06:27 -0300

    fix: conflicts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
