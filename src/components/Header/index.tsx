dbf203cd6abd77b46d91ad86eebb839ad743f386
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2022-10-30 13:31:15 -0300

    fix: display image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image';
-import Logo from '../../public/logo.png';
-
 export default function Header(): JSX.Element {
   return (
     <header>
-      <Image src={Logo} alt="logo" width={30} height={30} />
+      <img src="/logo.png" alt="logo" />
     </header>
   );
 }
