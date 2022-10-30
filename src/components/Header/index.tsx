78f57b9c3942411da886ddbf6ca66ae8321fd5bb
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2022-10-30 14:15:19 -0300

    chore: break style classes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import styles from './header.module.scss';
+import common from '../../styles/common.module.scss';
 
 export default function Header(): JSX.Element {
   return (
-    <header className={styles.container}>
+    <header className={`${styles.container} ${common.all}`}>
       <Link href="/">
         <img src="/logo.png" alt="logo" />
       </Link>
