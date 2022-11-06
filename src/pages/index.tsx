761f851c9e443c1d57983d89cca272a10b43f465
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2022-11-06 16:46:29 -0300

    feat: pagination home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import { useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 import Header from '../components/Header';
@@ -27,31 +28,40 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [newContentPagination, setNewContentPagination] =
+    useState<PostPagination>(postsPagination);
+
   async function handleFetchPaginationContent(): Promise<void> {
-    const newContent = await fetch(postsPagination.next_page, {
-      method: 'GET',
+    const newContent: PostPagination = await fetch(
+      newContentPagination.next_page,
+      {
+        method: 'GET',
+      }
+    ).then(data => data.json());
+
+    setNewContentPagination({
+      results: [...newContentPagination.results, ...newContent.results],
+      next_page: newContent.next_page,
     });
   }
 
   return (
-    <>
-      <main className={`${commonStyles.all} ${styles.main}`}>
-        <Header />
-        {postsPagination.results.map(post => (
-          <Cards
-            data={post.data}
-            first_publication_date={post.first_publication_date}
-            uid={post.uid}
-            key={post.uid}
-          />
-        ))}
-        {postsPagination.next_page && (
-          <button onClick={handleFetchPaginationContent} type="button">
-            Carregar mais posts
-          </button>
-        )}
-      </main>
-    </>
+    <main className={`${commonStyles.all} ${styles.main}`}>
+      <Header />
+      {newContentPagination.results.map(post => (
+        <Cards
+          data={post.data}
+          first_publication_date={post.first_publication_date}
+          uid={post.uid}
+          key={post.uid}
+        />
+      ))}
+      {newContentPagination.next_page && (
+        <button onClick={handleFetchPaginationContent} type="button">
+          Carregar mais posts
+        </button>
+      )}
+    </main>
   );
 }
 
@@ -59,7 +69,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const prismic = getPrismicClient({ req: params });
   const postsResponse = await prismic.getByType('posts', {
     fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
-    pageSize: 2,
+    pageSize: 1,
   });
 
   return {
